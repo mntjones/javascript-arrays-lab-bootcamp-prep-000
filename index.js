6d40003383ecb1733c1472884d2b23a42acbbc87
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-20 21:59:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
